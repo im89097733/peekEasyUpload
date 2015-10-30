f7e1512ac7a92634de95663153a3ed88be8a9b62
kevinleclair1 <kevinleclair1@gmail.com>
2015-10-30 10:25:44 -0400

    changed the function for increasing the database number to keep grouping

--- modules/utl.js
+++ modules/utl.js
@@ -4,8 +4,12 @@ function getVerFolderName(num){
 }
 
 //determine the new version number that needs to be returned to the server
+//need to keep first number for grouping in S3
 function increaseDbNum(num){
-
+	var groupNum = num.toString().slice(0,1);
+	var verNum = parseInt(num.toString().slice(1));
+	verNum++;
+	return parseInt(groupNum + verNum.toString());
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -2,16 +2,29 @@ var utl = require('./modules/utl.js');
 var expect = require('chai').expect;
 
 describe('Utl Tests', function(){
-	describe('Number Increases', function(){
+	describe('Get Folder Name String', function(){
 		it('gets proper folder name', function(){
 			var num = utl.getVerFolderName(10);
 			var num1 = utl.getVerFolderName(110);
-			var num2 = utl.getVerFolderName(234)
+			var num2 = utl.getVerFolderName(234);
 
 			expect(num).to.equal('ver 1.0');
 			expect(num1).to.equal('ver 1.10');
 			expect(num2).to.equal('ver 2.34');
-
 		});
 	});
-})
+	describe('Increases Number', function(){
+		it('works with base version 1.0', function(){
+			var num = utl.increaseDbNum(10);
+			expect(num).to.equal(11);
+		});
+		it('works with large numbers', function(){
+			var num = utl.increaseDbNum(1234);
+			expect(num).to.equal(1235);
+		});
+		it('doesn\'t change first number group', function(){
+			var num = utl.increaseDbNum(199);
+			expect(num).to.equal(1100);
+		});
+	});
+});
